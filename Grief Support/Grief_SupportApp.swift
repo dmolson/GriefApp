28756e58094202178264fce9c29f8179eaa90e5d
danielle-olson <danielle_olson@apple.com>
2025-08-01 00:30:32 -0700

    Initial Commit

---
+++ Grief Support/Grief_SupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grief_SupportApp.swift
+//  Grief Support
+//
+//  Created by Danielle Olson on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Grief_SupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Grief SupportTests/Grief_SupportTests.swift
@@ -0,0 +1,16 @@
+//
+//  Grief_SupportTests.swift
+//  Grief SupportTests
+//
+//  Created by Danielle Olson on 8/1/25.
+//
+
+import Testing
+
+struct Grief_SupportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
